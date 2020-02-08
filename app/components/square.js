d07fbe0374e5c67c5eb99d3cf9e1d7580598e4ed
emiel hagen <emiel.hagen@gmail.com>
2020-02-08 16:21:08 +0100

    added random x

--- app/components/square.js
+++ app/components/square.js
@@ -4,8 +4,12 @@ import { action } from '@ember/object';
 
 export default class SquareComponent extends Component {
   @tracked isO = false;
+  @tracked isX = false;
 
-  @action toggleSize() {
-    this.isO = !this.isO;
+  @action toggleO() {
+    this.isO = true;
+    let emptyBoxes = document.querySelectorAll('.square h2');
+    let random = Math.floor(Math.random()*emptyBoxes.length);
+    emptyBoxes[random].outerHTML = "<h3>X</h3>"
   }
 }
